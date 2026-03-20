30d040080ac1906ee746a90f463c5fc4495229d7
Luciano Pujol <150952795+lpujolSudameria@users.noreply.github.com>
2026-03-20 09:31:06 -0300

    Update footer.blade.php

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -1,120 +1,124 @@
-
-<footer class="bg-black text-white">
-    <div class="max-w-7xl mx-auto px-4 sm:px-4 py-3 text-center">
-
-        <div class="flex flex-wrap md:flex-nowrap md:justify-between gap-5 pt-5">
-            <div class="text-center mx-auto md:m-0 md:text-left w-full md:w-auto flex items-end gap-16">
-
-                <a href="https://sudameria.com" data-shortcut="home_view" class="mx-auto md:m-0">
-                    <img class="h-8" src="https://wiki.sudameria.com/images/logo_sdm.svg" alt="Logo">
+<footer class=" px-xl print-hidden text-center ">
+    <div class="container py-s flex-container-row gap-l wrap justify-space-between border-bottom-line ">
+        <div class="text-left ">
+            
+                <a href="{{ url('/') }}" data-shortcut="home_view" class="logo">
+                    @if (setting('app-logo-footer', '') !== 'none' && setting('app-logo-footer', '') !== '')
+                    {{--<img class="logo-image" src="{{ url(setting('app-logo-footer', '')) }}" alt="Logo">--}}
+                    @endif
+                    <img class="logo-image" src="{{  url('/images/logo_sdm.svg') }}" alt="Logo">
                 </a>
-
-            </div>
-
-
-
-            <div class="mx-auto md:m-0 text-center md:text-right w-full md:w-auto flex items-end ">
-
-                <ul class="flex mx-auto md:m-0 ">
-                    <li class="ms-4">
-                        <a target="_blank" href="https://www.facebook.com/Sudameriatravel">
-                            <i class="fa-brands fa-square-facebook text-lg"></i>
-                        </a>
-                    </li>
-                    <li class="ms-4">
-                        <a target="_blank" href="https://www.instagram.com/sudameriatravel/">
-                            <i class="fa-brands fa-instagram  text-lg"></i>
-                        </a>
-                    </li>
-
-                    <li class="ms-4">
-                        <a target="_blank" href="https://www.linkedin.com/company/sudameria-travel/"><i
-                                class="fa-brands fa-linkedin  text-lg"></i>
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-
+            
 
         </div>
 
-        <hr class="h-px my-8 bg-white border-0 ">
+        <div class="footer-social icons-min text-right ">
 
-        <div class="flex flex-wrap md:flex-nowrap md:justify-between items-center gap-5">
+            <ul>
+                <li>
+                    <a target="_blank" href="https://www.facebook.com/Sudameriatravel">
+                        <i class="lab la-facebook-square"></i>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" href="https://www.instagram.com/sudameriatravel/"><i
+                            class="lab la-instagram"></i></a>
+                </li>
 
-            <div class="h-6 w-full md:w-auto me-auto md:mx-0">
+                <li>
+                    <a target="_blank" href="https://www.linkedin.com/company/sudameria-travel/"><i
+                            class="lab la-linkedin"></i></a>
+                </li>
 
-                <img class="h-6 md:mx-0" src="https://wiki.sudameria.com/images/lufthansa.svg">
-
-            </div>
-            <div class="h-6 w-full md:w-auto me-auto md:mx-0">
-
-                <img class="h-4 md:mx-0" src="https://wiki.sudameria.com/images/Mesa-de-trabajo-2.svg">
-
-            </div>
-            <div class="h-6 w-full md:w-auto me-auto md:mx-0">
-
-                <img class="h-5  md:mx-0" src="https://wiki.sudameria.com/images/Mesa-de-trabajo-3.svg">
-
-            </div>
-            <div class="h-6 w-full md:w-auto me-auto md:mx-0">
-
-                <img class="h-5  md:mx-0" src="https://wiki.sudameria.com/images/Mesa-de-trabajo-4.svg">
-
-            </div>
-            <div class="h-6 w-full md:w-auto me-auto md:mx-0">
-
-                <img class="h-5  md:mx-0" src="https://wiki.sudameria.com/images/Mesa-de-trabajo-5.svg">
-
-            </div>
-
+            </ul>
         </div>
-        <hr class="h-px my-8 bg-white border-0 ">
 
-        <div class="flex flex-wrap md:flex-nowrap md:justify-between items-center gap-5">
-            <div class="w-full md:w-auto text-left text-white text-xs flex items-center gap-4">
-                <div class="text-white flex items-center">
-                    <x-logo-iso class="h-14 w-auto" />
-                </div>
-            </div>
-            <div class="w-full md:w-auto text-right text-white text-xs mt-auto">
-                <p>Lavalle 348, C1047, CABA, Argentina | 54.11.5218.6619 | <a href="#">info@sudameria.com.ar</a> | Legajo: EVT 11776</p>
-                                <p>© {{ \Carbon\Carbon::now()->year }} Sudameria Incoming Solutions SRL CUIT 33708398999</p>
 
-            </div>
-        </div>
     </div>
-    <div class="hidden">
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-900 text-sudameria-900 border border-sudameria-900 shadow-sudameria-lg ring ring-sudameria-900">
-            900</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-800 text-sudameria-800 border border-sudameria-800 shadow-sudameria-lg ring ring-sudameria-800">
-            800</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-700 text-sudameria-700 border border-sudameria-700 shadow-sudameria-lg ring ring-sudameria-700">
-            700</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-600 text-sudameria-600 border border-sudameria-600 shadow-sudameria-lg ring ring-sudameria-600">
-            600</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-500 text-sudameria-500 border border-sudameria-500 shadow-sudameria-lg ring ring-sudameria-500">
-            500</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-400 text-sudameria-400 border border-sudameria-400 shadow-sudameria-lg ring ring-sudameria-400">
-            400</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-300 text-sudameria-300 border border-sudameria-300 shadow-sudameria-lg ring ring-sudameria-300">
-            300</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-200 text-sudameria-200 border border-sudameria-200 shadow-sudameria-lg ring ring-sudameria-200">
-            200</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-100 text-sudameria-100 border border-sudameria-100 shadow-sudameria-lg ring ring-sudameria-100">
-            100</div>
-        <div
-            class="w-20 h-10 rounded-full bg-sudameria-50 text-sudameria-50 border border-sudameria-50 shadow-sudameria-lg ring ring-sudameria-50">
-            50</div>
+
+<style>
+
+.flex-container {
+    width: 100% !important;
+  display: flex;
+  justify-content: space-between;
+}
+
+.flex-container::before {
+  content: "";
+}
+
+.flex-container::after {
+  content: "";
+}
+
+/* non-essential decorative styles */
+.flex-container {
+  padding: 5px 0;
+  
+  
+}
+.flex-item {
+  height: 50px;
+  width: 75px;
+  
+}
+
+</style>
+
+    <div class="container py-xl flex-container-row gap-l wrap justify-space-between border-bottom-line external-logo">
+
+        <div class="flex-container">
+
+
+
+
+        <div class="flex-item">
+            <span>
+                <img src="/images/lufthansa.svg">
+            </span>
+        </div>
+        <div class="flex-item">
+            <span>
+                <img src="/images/Mesa-de-trabajo-2.svg">
+            </span>
+        </div>
+        <div class="flex-item">
+            <span>
+                <img src="/images/Mesa-de-trabajo-3.svg">
+            </span>
+        </div>
+        <div class="flex-item">
+            <span>
+                <img class="force-height" src="/images/Mesa-de-trabajo-4.svg">
+            </span>
+        </div>
+        <div class="flex-item">
+            <span>
+                <img src="/images/Mesa-de-trabajo-5.svg">
+            </span>
+        </div>
+
     </div>
+    </div>
+
+    <div class="container py-xl flex-container-row gap-l wrap justify-space-between bottom-text-footer">
+        <p>© 2021 Sudameria Incoming Solutions SRL CUIT 33708398999<br><span>Lavalle 348, C1047, CABA, Argentina | 54.11.5218.6619 | <a href="#">info@sudameria.com.ar</a></span>&nbsp;| Legajo: EVT 11776</p>
+    </div>
+
+    @if (count(setting('app-footer-links', [])) > 0)
+        <div id="foot-links">
+
+            <div class="text-center">
+
+
+                @foreach (setting('app-footer-links', []) as $link)
+                    <a href="{{ $link['url'] }}" target="_blank"
+                        rel="noopener">{{ strpos($link['label'], 'trans::') === 0 ? trans(str_replace('trans::', '', $link['label'])) : $link['label'] }}</a>
+                @endforeach
+
+
+            </div>
+        </div>
+    @endif
 </footer>
